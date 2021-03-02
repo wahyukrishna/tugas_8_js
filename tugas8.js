658492e821da55a2b4269d2ba549fb8592b3ce43
Wahyu Krishna <hanzo180895@gmail.com>
2021-03-02 14:03:40 +0700

    Tugas 8 JS
    Tugas Niomic

---
+++ tugas8.js
@@ -0,0 +1,14 @@
+const tugas8 = () => {
+    var motor ={
+        jenis: 'sport',
+        harga: 70000000,
+        warna: 'hitam',
+        tahun: [2014,2016,2018,2020]
+    }
+    console.log(motor);
+    console.log("");
+    motor.warna = 'kuning'
+    console.log(motor);
+}
+
+tugas8();
